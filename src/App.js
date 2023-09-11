74d0e7fa1a2bd93ae8ed323b04ea11858a3045af
Valentyna Astrakhovych <valentynaastrakhovych@gmail.com>
2023-09-11 17:34:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MainPage from  "./pages/Main/MainPage"
+
+
+
+
+export default function App() {
+
+  
+  return (
+    <div className="relative h-screen">
+      <MainPage />
+    </div>
+  );
+}
+
+
